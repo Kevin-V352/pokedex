74e96e48d4157eaba67397c2cc2d6096860e230c
kevin-v352 <kevinvega2070@gmail.com>
2021-10-19 02:30:11 -0300

    feat: added custom hook
    Added a custom hook to request pokemon API resources.
    
    developed-by: KV

---
+++ src/hooks/usePokemonPaginated.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useRef, useState } from 'react';
+
+import { pokemonApi } from '../api/pokemonAPI';
+
+import { PokemonPaginatedResponse, SinglePokemonResponse } from '../interfaces/pokemonsInterfaces';
+
+const usePokemonPaginated = () => {
+
+  const nextPageUrl = useRef<string>('https://pokeapi.co/api/v2/pokemon?limit=20');
+
+  const [pokemonList, setPokemonList] = useState<SinglePokemonResponse[]>([]);
+
+  const loadPokemons = async () => {
+  
+    const resp = await pokemonApi.get<PokemonPaginatedResponse>(nextPageUrl.current);
+    nextPageUrl.current = resp.data.next;
+
+    const PromisesPokemonDetails = resp.data.results.map(({ url }) => (
+      pokemonApi.get<SinglePokemonResponse>(url)
+    ));
+    const pokemonDetailsResponse = await Promise.all(PromisesPokemonDetails);
+    const pokemonDetails = pokemonDetailsResponse.map((item) => item.data);
+
+    setPokemonList((previusState) => ([...previusState, ...pokemonDetails]));
+  };
+
+  useEffect(() => {
+    loadPokemons();
+  }, [])
+
+  return {
+    pokemonList,
+    loadPokemons
+  };
+};
+
+
+export default usePokemonPaginated;
+
