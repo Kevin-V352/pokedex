7dec3bcec1c6981be9b8ca1af2acf50b73ef9d92
kevin-v352 <kevinvega2070@gmail.com>
2021-10-19 02:02:24 -0300

    fate: added additional elements to the homescreen
    Added vertical list with pokemon and background image.
    
    developed-by: KV

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,50 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, StyleSheet, Image, FlatList } from 'react-native';
+
+import {widthPercentageToDP as wp } from 'react-native-responsive-screen';
+import PokemonCard from '../components/PokemonCard';
+
+import usePokemonPaginated from '../hooks/usePokemonPaginated';
 
 const HomeScreen = () => {
+
+  const { pokemonList, loadPokemons } = usePokemonPaginated()
+  
   return (
-    <View>
-      <Text>hola mundo</Text>
+    <View style={styles.container}>
+      <Image
+        source={require('../../assets/images/half-pokeball.png')}
+        style={styles.topLogo}
+      />
+      <FlatList
+        data={pokemonList}
+        renderItem={({ item }) => <PokemonCard pokemon={item}/>}
+        contentContainerStyle={{
+          alignItems: 'center',
+          width: wp(100),
+          paddingVertical: 30
+        }}
+        showsVerticalScrollIndicator={false}
+        onEndReached={loadPokemons}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: 'white'
+  },
+  topLogo: {
+    resizeMode: 'stretch',
+    height: wp(90),
+    width: wp(90),
+    position: 'absolute',
+    top: wp(-48),
+    left: wp(5)
+  }
+});
 
 export default HomeScreen;
