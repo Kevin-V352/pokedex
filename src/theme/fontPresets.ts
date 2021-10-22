1a76cce1eedfa85359dc4be2df68463d3c85c219
kevin-v352 <kevinvega2070@gmail.com>
2021-10-22 02:20:15 -0300

    fix: changed the settings for new font size
    Added a new intermediate font size between secondary and quaternary.
    
    Corrected-by: KV

--- src/theme/fontPresets.ts
+++ src/theme/fontPresets.ts
@@ -4,7 +4,8 @@ const fontPresets = {
   sizes: {
     primarySize: hp(4),
     secondarySize: hp(3),
-    tertiarySize: hp(2)
+    tertiarySize: hp(2.5),
+    quaternarySize: hp(2)
   },
   weights: {
     bold: 'Quicksand-Bold',
