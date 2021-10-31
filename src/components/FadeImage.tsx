024cd96683d79125693220d4443cc826d7527587
kevin-v352 <kevinvega2070@gmail.com>
2021-10-31 19:23:29 -0300

    feat: added new component for images
    A component was created to load images in an animated way.
    
    Developed-By: KV

---
+++ src/components/FadeImage.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Animated, ImageStyle, StyleProp, ViewStyle } from 'react-native';
+
+import FastImage from 'react-native-fast-image';
+
+import useFadeAnimation from '../hooks/useFadeAnimation';
+
+interface Props {
+  uri: string;
+  customImageStyles: StyleProp<ImageStyle>;
+  customContainerStyles: StyleProp<ViewStyle>;
+};
+
+const FadeImage = ({ uri, customImageStyles, customContainerStyles }: Props) => {
+  const { opacity, fadeIn } = useFadeAnimation();
+
+  return (
+    <Animated.View style={{
+      ...customContainerStyles as any,
+      opacity
+    }}
+    >
+      <FastImage
+        source={{
+          uri,
+          priority: FastImage.priority.high
+        }}
+        style={customImageStyles as any}
+        onLoad={fadeIn}
+      />
+    </Animated.View>
+  );
+};
+
+export default FadeImage;
