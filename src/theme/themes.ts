d8b8c0a91c8d63e5aafb9919ff91fd9df430cd84
kevin-v352 <kevinvega2070@gmail.com>
2021-10-30 21:35:40 -0300

    fix: updated global palette
    An unused color was removed.
    
    Corrected-by: KV

--- src/theme/themes.ts
+++ src/theme/themes.ts
@@ -3,15 +3,13 @@ const themes = {
     name: 'light',
     primaryColor: '#1C1C1C',
     secondaryColor: 'rgba(0, 0, 0, 0.5)',
-    tertiaryColor: '#FFFFFF',
-    typeColor: '#FFFFFF'
+    tertiaryColor: '#FFFFFF'
   },
   dark: {
     name: 'dark',
     primaryColor: '#FFFFFF',
     secondaryColor: 'rgba(255, 255, 255, 0.5)',
-    tertiaryColor: '#1C1C1C',
-    typeColor: '#1C1C1C'
+    tertiaryColor: '#1C1C1C'
   }
 };
 
