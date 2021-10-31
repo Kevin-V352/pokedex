ec80e70576ff3547ba14f336cbddbec17e8288c4
kevin-v352 <kevinvega2070@gmail.com>
2021-10-31 19:18:49 -0300

    Feat: Updated Home Screen Performance
    Updated the list parameters on the home screen to optimize the performance of the list, a loading icon was also added in the footer.
    
    Developed-By: KV

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,40 +1,59 @@
-import React, { useContext } from 'react';
-import { View, StyleSheet, Image, FlatList } from 'react-native';
+import React, { useCallback, useContext } from 'react';
+import { View, StyleSheet, Image, FlatList, StatusBar } from 'react-native';
 
-import { widthPercentageToDP as wp } from 'react-native-responsive-screen';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp
+} from 'react-native-responsive-screen';
 
+import Loader from '../components/Loader';
 import PokemonCard from '../components/PokemonCard';
 import { ThemeContext } from '../contexts/themeContext/ThemeContext';
 import usePokemonPaginated from '../hooks/usePokemonPaginated';
 
+const darkImage = require('../../assets/images/pokeball_icon_dark.png');
+const lightImage = require('../../assets/images/pokeball_icon_light.png');
+
+const getItemLayout = (data: any, index: number) => (
+  { length: hp(19), offset: (hp(19) * index), index }
+);
+
 const HomeScreen = () => {
-  const { currentTheme } = useContext(ThemeContext);
+  const { currentTheme: { name, tertiaryColor } } = useContext(ThemeContext);
+
+  const renderItem = useCallback(({ item }) => <PokemonCard pokemon={item} />, []);
+  const keyExtractor = useCallback(({ id }) => id.toString(), []);
+  const renderFooterLoader = useCallback(() => <Loader customStyles={styles.footerLoader} />, []);
 
   const { pokemonList, loadPokemons } = usePokemonPaginated();
+
+  const lightThemeEnabled = (name === 'light');
 
   return (
     <View
       style={{
         ...styles.container,
-        backgroundColor: currentTheme.tertiaryColor
+        backgroundColor: tertiaryColor
       }}
     >
+      <StatusBar
+        animated
+        backgroundColor={lightThemeEnabled ? '#E5E5E5' : '#323232'}
+        barStyle={lightThemeEnabled ? 'dark-content' : 'light-content'}
+      />
       <Image
-        // eslint-disable-next-line global-require
-        source={require('../../assets/images/half-pokeball.png')}
+        source={lightThemeEnabled ? lightImage : darkImage}
         style={styles.topLogo}
       />
       <FlatList
         data={pokemonList}
-        renderItem={({ item }) => <PokemonCard pokemon={item} />}
-        keyExtractor={(item) => item.id.toString()}
-        contentContainerStyle={{
-          alignItems: 'center',
-          width: wp(100),
-          paddingVertical: 30
-        }}
+        renderItem={renderItem}
+        keyExtractor={keyExtractor}
         showsVerticalScrollIndicator={false}
         onEndReached={loadPokemons}
+        ListFooterComponent={renderFooterLoader}
+        getItemLayout={getItemLayout}
+        contentContainerStyle={styles.listContianerStyle}
       />
     </View>
   );
@@ -51,7 +70,16 @@ const styles = StyleSheet.create({
     width: wp(90),
     position: 'absolute',
     top: wp(-48),
-    left: wp(5)
+    left: wp(5),
+    opacity: 0.2
+  },
+  listContianerStyle: {
+    alignItems: 'center',
+    width: wp(100)
+  },
+  footerLoader: {
+    height: hp(22),
+    flex: 0
   }
 });
 
