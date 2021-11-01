45e2f91a888cfcbed39e5bc41139ae2fb43f0e63
kevin-v352 <kevinvega2070@gmail.com>
2021-11-01 20:06:51 -0300

    feat: updated browser styles in detailscreen
    Styles have been updated in the browser interface.
    
    Developed-By: KV

--- src/navigators/MaterialTopTabNavigator.tsx
+++ src/navigators/MaterialTopTabNavigator.tsx
@@ -1,12 +1,12 @@
 /* eslint-disable react/no-children-prop */
 
 import React, { useContext } from 'react';
+import { StyleSheet } from 'react-native';
 
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
 import { ThemeContext } from '../contexts/themeContext/ThemeContext';
 import elementsPalette from '../data/elementsPalette';
-import { calculateWidth } from '../helpers/dimensionFormatters';
 import { PokemonResponse } from '../interfaces/pokemonsInterfaces';
 import AboutScreen from '../screens/AboutScreen';
 import EvolutionScreen from '../screens/EvolutionScreen';
@@ -22,47 +22,51 @@ const Tab = createMaterialTopTabNavigator();
 const MaterialTopTabNavigator = ({ pokemon }: Props) => {
   const typeColor: string = elementsPalette[pokemon.types[0].type.name];
 
-  const { currentTheme } = useContext(ThemeContext);
+  const { currentTheme: { secondaryColor, tertiaryColor } } = useContext(ThemeContext);
 
   return (
     <Tab.Navigator
       backBehavior="none"
-      sceneContainerStyle={{ backgroundColor: 'white' }}
       screenOptions={{
         lazy: true,
-
-        tabBarLabelStyle: {
-          textTransform: 'none',
-          fontFamily: fontPresets.weights.semiBold,
-          fontSize: fontPresets.sizes.secondarySize
-        },
-        /* tabBarItemStyle: { backgroundColor: 'red', alignSelf: 'flex-end' }, */
+        tabBarContentContainerStyle: { backgroundColor: tertiaryColor },
+        tabBarIndicatorStyle: styles.tabBarIndicatorStyle,
+        tabBarLabelStyle: styles.tabBarLabelStyle,
+        tabBarActiveTintColor: typeColor,
+        tabBarInactiveTintColor: secondaryColor,
         tabBarStyle: {
           // Remove shaded border in IOS
-          shadowColor: currentTheme.secondaryColor,
+          shadowColor: secondaryColor,
           // Remove shaded border in Android
           elevation: 0
         },
         tabBarPressColor: 'transparent'
       }}
+      sceneContainerStyle={{ backgroundColor: tertiaryColor }}
     >
       <Tab.Screen
         name="About"
         children={() => <AboutScreen pokemon={pokemon} />}
-        options={{ tabBarIndicatorStyle: calculateWidth('Stats', typeColor) }}
       />
       <Tab.Screen
         name="Stats"
         children={() => <StatsScreen pokemon={pokemon} />}
-        options={{ tabBarIndicatorStyle: calculateWidth('Stats', typeColor) }}
       />
       <Tab.Screen
         name="Evolution"
         children={() => <EvolutionScreen pokemon={pokemon} />}
-        options={{ tabBarIndicatorStyle: calculateWidth('Evolution', typeColor) }}
       />
     </Tab.Navigator>
   );
 };
+
+const styles = StyleSheet.create({
+  tabBarLabelStyle: {
+    textTransform: 'none',
+    fontFamily: fontPresets.weights.bold,
+    fontSize: fontPresets.sizes.secondarySize
+  },
+  tabBarIndicatorStyle: { backgroundColor: 'transparent' }
+});
 
 export default MaterialTopTabNavigator;
