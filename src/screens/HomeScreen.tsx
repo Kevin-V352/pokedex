5e49eff61aec42b47e28cce730f1d474745c15c4
kevin-v352 <kevinvega2070@gmail.com>
2021-11-04 13:41:39 -0300

    feat: updated "HomeScreen" display
    Removed the "FlatList" for the standalone component "ListOfPokemons", also added a search bar to filter pokemons by name.
    
    Developed-by: KV

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,33 +1,55 @@
-import React, { useCallback, useContext } from 'react';
-import { View, StyleSheet, Image, FlatList, StatusBar } from 'react-native';
+import React, { useContext, useEffect, useState } from 'react';
+import { View, StyleSheet, StatusBar } from 'react-native';
 
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp
 } from 'react-native-responsive-screen';
 
-import Loader from '../components/Loader';
-import PokemonCard from '../components/PokemonCard';
+import AppText from '../components/AppText';
+import CustomIcon from '../components/CustomIcon';
+import ListOfPokemons from '../components/ListOfPokemons';
+import SearchBar from '../components/SearchBar';
 import { ThemeContext } from '../contexts/themeContext/ThemeContext';
-import usePokemonPaginated from '../hooks/usePokemonPaginated';
-
-const darkImage = require('../../assets/images/pokeball_icon_dark.png');
-const lightImage = require('../../assets/images/pokeball_icon_light.png');
-
-const getItemLayout = (data: any, index: number) => (
-  { length: hp(19), offset: (hp(19) * index), index }
-);
+import usePokemonsPaginated from '../hooks/usePokemonsPaginated';
+import useSearchPokemons from '../hooks/useSearchPokemons';
+import fontPresets from '../theme/fontPresets';
 
 const HomeScreen = () => {
-  const { currentTheme: { name, tertiaryColor } } = useContext(ThemeContext);
+  const [searchTerm, setSearchTerm] = useState<string>('');
 
-  const renderItem = useCallback(({ item }) => <PokemonCard pokemon={item} />, []);
-  const keyExtractor = useCallback(({ id }) => id.toString(), []);
-  const renderFooterLoader = useCallback(() => <Loader customStyles={styles.footerLoader} />, []);
+  const { currentTheme: { name, primaryColor, secondaryColor, tertiaryColor } } = useContext(ThemeContext);
 
-  const { pokemonList, loadPokemons } = usePokemonPaginated();
+  const { pokemonsListPaged, loadingPaginatedList, loadPaginatedListItems } = usePokemonsPaginated();
+  const {
+    pokemonsSearched,
+    loadingSearchItems,
+    lengthOfAllPokemons,
+    lenghtPokemonsExtracted,
+    filterPokemons,
+    loadSearchItems
+  } = useSearchPokemons();
 
   const lightThemeEnabled = (name === 'light');
+  const searchEnabled = (searchTerm.length > 0);
+
+  useEffect(() => {
+    filterPokemons(searchTerm);
+  }, [searchTerm]);
+
+  if (loadingPaginatedList || loadingSearchItems) {
+    return (
+      <View style={{
+        backgroundColor: 'gray',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+      >
+        <AppText text="Cargando" />
+      </View>
+    );
+  };
 
   return (
     <View
@@ -41,19 +63,31 @@ const HomeScreen = () => {
         backgroundColor={lightThemeEnabled ? '#E5E5E5' : '#323232'}
         barStyle={lightThemeEnabled ? 'dark-content' : 'light-content'}
       />
-      <Image
-        source={lightThemeEnabled ? lightImage : darkImage}
-        style={styles.topLogo}
+      <CustomIcon
+        name="pokeball"
+        size={hp(30)}
+        color={secondaryColor}
+        style={styles.pokeballIcon}
       />
-      <FlatList
-        data={pokemonList}
-        renderItem={renderItem}
-        keyExtractor={keyExtractor}
-        showsVerticalScrollIndicator={false}
-        onEndReached={loadPokemons}
-        ListFooterComponent={renderFooterLoader}
-        getItemLayout={getItemLayout}
-        contentContainerStyle={styles.listContianerStyle}
+      <AppText
+        text="Pokédex"
+        customStyles={{
+          ...styles.appTitle,
+          color: primaryColor
+        }}
+      />
+      <AppText
+        text="Search for Pokémon by name or using the National Pokédex number."
+        customStyles={{
+          ...styles.desc,
+          color: secondaryColor
+        }}
+      />
+      <SearchBar onDebounce={setSearchTerm} />
+      <ListOfPokemons
+        data={searchEnabled ? pokemonsSearched : pokemonsListPaged}
+        numberOfItems={searchEnabled ? lenghtPokemonsExtracted.current : lengthOfAllPokemons.current}
+        onEndReached={searchEnabled ? loadSearchItems : loadPaginatedListItems}
       />
     </View>
   );
@@ -64,22 +98,24 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center'
   },
-  topLogo: {
-    resizeMode: 'stretch',
-    height: wp(90),
-    width: wp(90),
+  pokeballIcon: {
     position: 'absolute',
-    top: wp(-48),
-    left: wp(5),
-    opacity: 0.2
+    opacity: 0.2,
+    right: hp(-10),
+    top: hp(-10),
+    transform: [{ rotate: '145deg' }]
   },
-  listContianerStyle: {
-    alignItems: 'center',
-    width: wp(100)
+  appTitle: {
+    fontSize: fontPresets.sizes.primarySize,
+    fontFamily: fontPresets.weights.bold,
+    marginTop: wp(10),
+    marginBottom: hp(2),
+    width: wp(90)
   },
-  footerLoader: {
-    height: hp(22),
-    flex: 0
+  desc: {
+    fontSize: fontPresets.sizes.tertiarySize,
+    textAlign: 'justify',
+    width: wp(90)
   }
 });
 
